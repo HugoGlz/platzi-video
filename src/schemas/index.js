ec7a6615823d7d2df2cd18cb5bf3acb2608b510a
Hugo Gonzalez <hgonzalez@innomius.com>
2018-11-13 08:43:07 -0600

    normalize

---
+++ src/schemas/index.js
@@ -0,0 +1,13 @@
+import api from '../api.json';
+
+import {normalize, schema} from 'normalizr';
+
+const media = new shema.Entity();
+
+const category = new shema.Entity();
+
+const categories = 
+
+const normalizeData = normalize(api, categories);
+
+export default normalizeData;
